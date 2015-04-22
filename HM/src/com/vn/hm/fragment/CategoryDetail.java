87344103b5b54656da5654b2ebaf975969a58f50
dodanhhieu <dodanhhieu@gmail.com>
2015-04-22 18:25:35 +0700

    uu

---
+++ HM/src/com/vn/hm/fragment/CategoryDetail.java
@@ -0,0 +1,26 @@
+package com.vn.hm.fragment;
+
+import android.view.View;
+import android.widget.ListView;
+
+import com.d3.base.BaseFragment;
+import com.vn.hm.R;
+
+public class CategoryDetail extends BaseFragment {
+
+	private String TAG = "CategoryDetail";
+	private ListView listview;
+	
+	@Override
+	public int getlayout() {
+		
+		return R.layout.category_detail_layout;
+	}
+
+	@Override
+	public void initView(View view) {
+		
+		
+	}
+
+}
---
+++ HM/src/com/vn/hm/object/CategoryObjectDetail.java
@@ -0,0 +1,60 @@
+package com.vn.hm.object;
+
+public class CategoryObjectDetail {
+
+	private int id;
+	private String title;
+	private String description;
+	private String content;
+	private String image;
+	private String video;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public String getImage() {
+		return image;
+	}
+
+	public void setImage(String image) {
+		this.image = image;
+	}
+
+	public String getVideo() {
+		return video;
+	}
+
+	public void setVideo(String video) {
+		this.video = video;
+	}
+
+}
