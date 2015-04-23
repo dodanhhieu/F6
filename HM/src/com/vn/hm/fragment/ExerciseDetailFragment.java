d5221962c1a9a2dd1cc1d5700a425bddbef74279
dodanhhieu <dodanhhieu@gmail.com>
2015-04-23 17:15:05 +0700

    cate detail

--- HM/src/com/vn/hm/fragment/ExerciseDetailFragment.java
+++ HM/src/com/vn/hm/fragment/ExerciseDetailFragment.java
@@ -1,12 +1,25 @@
 package com.vn.hm.fragment;
 
+import java.util.HashMap;
+
+import android.util.Log;
 import android.view.View;
 
 import com.d3.base.BaseFragment;
+import com.d3.base.D3Utils;
+import com.vn.base.api.ApiServiceCallback;
 import com.vn.hm.R;
+
+import d3.lib.base.callback.RestClient.RequestMethod;
 
 public class ExerciseDetailFragment extends BaseFragment{
 
+	private String TAG = "ExerciseDetailFragment";
+	private int idCate;
+	public ExerciseDetailFragment(int idCate) {
+		this.idCate = idCate;
+	}
+	
 	@Override
 	public int getlayout() {
 		
@@ -17,5 +30,5 @@ public class ExerciseDetailFragment extends BaseFragment{
 	public void initView(View view) {
 		
 	}
-
-}
+	
+	}
