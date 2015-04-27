34cd0343acea285d8f6ca97cac9a4c3e55327f02
dodanhhieu <dodanhhieu@gmail.com>
2015-04-27 08:42:39 +0700

    ok

---
+++ HM/src/com/vn/hm/fragment/CalendarFragment.java
@@ -0,0 +1,21 @@
+package com.vn.hm.fragment;
+
+import android.view.View;
+
+import com.d3.base.BaseFragment;
+
+public class CalendarFragment extends BaseFragment{
+
+	@Override
+	public int getlayout() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void initView(View view) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
